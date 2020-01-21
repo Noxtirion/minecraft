a3bf3ef60f8aac55f742fc2a58cf0f07b9cf16ce
Darek <dardman88@gmail.com>
2020-01-21 22:24:36 +0100

    Make changes for future site improvement

---
+++ README.md
@@ -0,0 +1,18 @@
+# Minecraft-serv
+
+> A page that shows regulations and informations about Minecraft server.
+
+## General info
+
+A non-profit page which I was asked to prepare in order to introduce general rules to community.
+
+## Technologies
+
+-  JavaScript ES6
+-  HTML 5
+-  SCSS
+
+## Methodology
+
+-  BEM
+
---
+++ main.js
@@ -0,0 +1,123 @@
+window.addEventListener("load", () => {
+   const headerBig =
+      document.querySelector(".header__title--big") === null
+         ? false
+         : document.querySelector(".header__title--big");
+   const headerSmall =
+      document.querySelector(".header__title--small") === null
+         ? false
+         : document.querySelector(".header__title--small");
+
+   headerBig && headerBig.classList.toggle("header__title--big-grow");
+
+   headerSmall && headerSmall.classList.toggle("header__title--small-grow");
+});
+
+////observer
+
+const ladders = document.querySelectorAll(".article__image--ladder");
+const figures = document.querySelectorAll(".article__image--figure");
+const anchor = document.getElementById("top");
+const buttonUp = document.querySelector(".button-top");
+
+const options = {
+   rootMargin: "0px 0px -150px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+   entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+         return;
+      } else {
+         entry.target.classList.add("appear");
+         appearOnScroll.unobserve(entry.target);
+      }
+   });
+}, options);
+
+ladders.forEach(fader => {
+   appearOnScroll.observe(fader);
+});
+
+///////////////////////////////////
+
+const figuresOptions = {
+   threshold: 0,
+   rootMargin: "0px 0px -150px 0px"
+};
+
+const appearOnScrollFigures = new IntersectionObserver(function(entries, appearOnScrollFigures) {
+   entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+         entry.target.classList.remove("appear");
+      } else {
+         entry.target.classList.add("appear");
+      }
+   });
+}, figuresOptions);
+
+figures.forEach(fader => {
+   appearOnScrollFigures.observe(fader);
+});
+
+//////////////////////////////////
+
+const buttonOptions = {
+   rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScrollButton = new IntersectionObserver(function(entries, appearOnScrollButton) {
+   entries.forEach(entry => {
+      if (entry.isIntersecting) {
+         buttonUp.classList.remove("appear");
+      } else {
+         buttonUp.classList.add("appear");
+      }
+   });
+}, buttonOptions);
+
+appearOnScrollButton.observe(anchor);
+
+//////// Smooth scroll
+
+$(".menu__links a").smoothScroll({
+   offset: 0,
+   afterScroll: function() {
+      $(this)
+         .closest(".menu__links")
+         .find("a")
+         .removeClass("active");
+      $(this).addClass("active");
+   }
+});
+
+$(".button-top a").smoothScroll({
+   offset: 0,
+   afterScroll: function() {
+      $(this)
+         .closest(".button-top")
+         .find("a")
+         .removeClass("active");
+      $(this).addClass("active");
+   }
+});
+
+//////// Menu Button
+
+const menuButton = document.querySelector(".menu__button");
+const menuLinks = document.querySelector(".menu__links");
+const toggleMenu = () => {
+   if (menuButton != null) {
+      menuButton.addEventListener("click", function() {
+         if (this.innerHTML === "Open") {
+            this.innerHTML = "Close";
+            menuLinks.classList.add("menu__links--open");
+         } else {
+            this.innerHTML = "Open";
+            menuLinks.classList.remove("menu__links--open");
+         }
+      });
+   }
+   return;
+};
+toggleMenu();
